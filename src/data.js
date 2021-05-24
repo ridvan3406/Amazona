d388e0bd7b692db8b314224e99f5b54268456e76
ridvan3406 <ridvansaman34@gmail.com>
2021-05-24 03:15:34 +0100

    css and html contents migrated to react component and products been mapped

---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data={
+    products:[
+    {
+        _id:'1',
+        name:"Nike Slim Shirt",
+        category:"Shirts",
+        image:'./images/p1.jpg',
+        price:120,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:"highly quality product"
+    },
+    {
+        _id:'2',
+        name:"Adidas Slim Shirt",
+        category:"Shirts",
+        image:'./images/p2.jpg',
+        price:100,
+        brand:'Adidas',
+        rating:4.0,
+        numReviews:101,
+        description:"highly quality product"
+    },
+    {
+        _id:'3',
+        name:"Lacoste  Shirt",
+        category:"Shirts",
+        image:'./images/p3.jpg',
+        price:130,
+        brand:'Lacoste',
+        rating:3.5,
+        numReviews:40,
+        description:"quality product"
+    },
+    {
+        _id:'4',
+        name:"Reebook Slim Pant",
+        category:"Pants",
+        image:'./images/p4.jpg',
+        price:20,
+        brand:'Reebook',
+        rating:5.0,
+        numReviews:440,
+        description:"highly quality product"
+    },
+    {
+        _id:'5',
+        name:"Pierre Cardin Slim Pants",
+        category:"Pants",
+        image:'./images/p5.jpg',
+        price:220,
+        brand:'Pierre Cardin',
+        rating:5.0,
+        numReviews:50,
+        description:"highly quality product"
+    },
+    {
+        _id:'6',
+        name:"Diesel Slim Pant",
+        category:"Pants",
+        image:'./images/p6.jpg',
+        price:22,
+        brand:'Diesel',
+        rating:3.0,
+        numReviews:53,
+        description:"highly quality product"
+    },
+    
+]
+}
+export default data;
