3722ff5ea353a346bb184adc98b3d1c865710180
ridvan3406 <ridvansaman34@gmail.com>
2021-05-24 04:14:03 +0100

    rating and reviews comp added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import React from 'react'
 import data from './data'
+import Product from './components/Product'
 
-function App() {
+
+function App(props) {
   return (
     <div className="grid-container">
       <header className="row">
@@ -17,25 +19,8 @@ function App() {
       <main>
         <div>
           <div className="row center">
-            {data.products.map(product=>(<div key={product._id} className="card">
-               <a href={`/product/${product._id}`}>
-                 <img className="medium" 
-                 src={product.image} alt={product.name} />
-               </a>
-               <div className="card-body">
-                 <a href={`/product/${product._id}`}>
-                   <h2>{product.name}</h2>
-                 </a>
-                 <div className="rating">
-                   <span> <i className="fa fa-star"></i> </span>
-                   <span> <i className="fa fa-star"></i> </span>
-                   <span> <i className="fa fa-star"></i> </span>
-                   <span> <i className="fa fa-star"></i> </span>
-                   <span> <i className="fa fa-star"></i> </span>
-                 </div>
-                 <div className="price">${product.price}</div>
-               </div> 
-              </div>
+            {data.products.map(product=>(
+              <Product key={product._id} product={product}/>
               ))}
           </div>
           </div>
